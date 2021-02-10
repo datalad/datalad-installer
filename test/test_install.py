64c29c1a4869375996c051d3b58ace023857ba72
John T. Wodder II <git@varonathe.org>
2021-02-10 14:54:23 -0500

    Set TMPDIR to C:\tmp on Windows

--- test/test_install.py
+++ test/test_install.py
@@ -41,26 +41,30 @@ def test_install_miniconda(tmp_path):
     assert "conda activate test" in r.stdout
 
 
-def test_install_miniconda_autogen_path(monkeypatch, tmp_path):
-    monkeypatch.setenv("TMPDIR", str(tmp_path))
-    tempfile.tempdir = None  # Reset cache
-    r = main(
-        [
-            "datalad_installer.py",
-            "miniconda",
-            "--batch",
-        ]
-    )
-    assert r == 0
-    (miniconda_path,) = tmp_path.glob("dl-miniconda-*")
-    assert (miniconda_path / bin_path("conda")).exists()
-    r = subprocess.run(
-        [str(miniconda_path / bin_path("conda")), "create", "-n", "test", "-y"],
-        stdout=subprocess.PIPE,
-        universal_newlines=True,
-        check=True,
-    )
-    assert "conda activate test" in r.stdout
+def test_install_miniconda_autogen_path(monkeypatch):
+    # Override TMPDIR with a path that will be cleaned up afterwards (We can't
+    # use tmp_path here, as that's apparently always in the user temp folder on
+    # Windows regardless of the external value of TMPDIR.)
+    with tempfile.TemporaryDirectory() as newtmp:
+        monkeypatch.setenv("TMPDIR", newtmp)
+        tempfile.tempdir = None  # Reset cache
+        r = main(
+            [
+                "datalad_installer.py",
+                "miniconda",
+                "--batch",
+            ]
+        )
+        assert r == 0
+        (miniconda_path,) = Path(newtmp).glob("dl-miniconda-*")
+        assert (miniconda_path / bin_path("conda")).exists()
+        r = subprocess.run(
+            [str(miniconda_path / bin_path("conda")), "create", "-n", "test", "-y"],
+            stdout=subprocess.PIPE,
+            universal_newlines=True,
+            check=True,
+        )
+        assert "conda activate test" in r.stdout
     tempfile.tempdir = None  # Reset cache
 
 
